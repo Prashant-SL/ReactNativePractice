f5db8c1807ec390943140058920285f839291258
Prashant-SL <pacchidon07@gmail.com>
2022-07-19 08:38:33 +0530

    JSX Elements

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,14 @@
-// import { View, Text } from 'react-native'
-// import React from 'react'
-
-// const Home = () => {
-//   return (
-//     <View>
-//       <Text>Home</Text>
-//     </View>
-//   )
-// }
-// export default Home;
-
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Home = () => {
+    let name = "Prashant SL";
     return (
         <View style={styles.container}>
-            <Text style={styles.textColor}>Open up Home.js now bro to start working and queen and childrens hi bro not fast??? on your app!</Text>
             <StatusBar style="auto" />
+            <Text style={styles.textColor}>Welcome {name} Bro!!!!</Text>
+            <Text style={styles.textColor2}>We love React Native Bro!!!!</Text>
+            <Text>Hi!!!! My name is {name} Bro!!!!</Text>
         </View>
     );
 }
@@ -35,8 +25,16 @@ const styles = StyleSheet.create({
     textColor: {
         alignItems: 'center',
         justifyContent: 'center',
-        color: "red",
-        backgroundColor: "yellow"
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "blue",
+    },
+    textColor2: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "blue",
     },
     box: {
         margin: 0,
