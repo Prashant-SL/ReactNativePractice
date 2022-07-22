9cd273f5b2d5f8985c3933e22d7842ba67560149
Prashant-SL <pacchidon07@gmail.com>
2022-07-22 22:39:40 +0530

    Card2

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import Card from './src/Components/Card';
 import TouchButton from './src/Components/TouchButton';
 
 function App() {
+  let arr = [{ a: "a" }, { b: "b" }, { c: "c" }, { d: "d" }, { e: "e" }]
+  // let arr=[1,2,3,4,5];
   return (
     <View style={styles.container}>
       {/* <TouchButton /> */}
-      <Card />
+      <FlatList data={arr} renderItem={(e) => {
+        return (
+          <Card />
+        )
+      }} />
+      {/* <Card /> */}
     </View>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,11 +7,11 @@ const Card = () => {
             borderWidth: 1, alignItems: "center", width: 250, marginHorizontal: 55,
             justifyContent: "flex-start"
         }}>
-            <Image style={{ width: "100%", height: "50%", marginBottom: 30 }} source={{ uri: "https://reactjs.org/logo-og.png/" }} />
-            <View style={{ textAlign: "center" }}>
+            <Image style={{ width: "100%", height: "50%", marginBottom: 30, }} source={{ uri: "https://reactjs.org/logo-og.png/" }} />
+            <View style={{ textAlign: "center", }}>
                 <Text style={{ fontWeight: "bold", fontSize: 18, color: "skyblue", textAlign: "left" }}>All Of Us Are Dead</Text>
-                <Text style={{}}>Card Title</Text>
-                <Text>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</Text>
+                <Text style={{ paddingLeft: 10 }}>Card Title </Text>
+                <Text style={{ paddingLeft: 2 }}>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</Text>
             </View>
         </View>
     );
